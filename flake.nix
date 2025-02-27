307441f96d85ddc99766d7ae4a1e974bf2882abf
Sense T <me@sense-t.eu.org>
2025-02-27 23:35:05 +0800

    init

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "";
+  
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs =
+    { nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+      packages-inode = with pkgs; [
+        libgcc
+        libxcrypt-legacy
+        libuuid
+        libpng12
+        libjpeg
+        libudev0-shim
+        libz
+        atk
+        ell
+        glib
+        cairo
+        ncurses5
+        pango
+        gtk2
+        gdk-pixbuf
+        freetype
+        fontconfig
+        xorg.libSM
+        xorg.libX11
+        xorg.libXxf86vm
+
+      ];
+      inherit (pkgs) mkShell;
+    in
+    {
+      formatter.${system} = treefmtEval.config.build.wrapper;
+      devShell."${system}" = mkShell {
+        buildInputs =
+          with pkgs;
+          [
+            tokei
+          ]
+          ++ packages-inode;
+      };
+
+      packages."${system}" = {
+        dev-inode =
+          let
+            pkgs = import nixpkgs { inherit system; };
+          in
+          pkgs.buildFHSEnv {
+            name = "inode-dev-shell";
+            buildInputs = packages-inode;
+            targetPkgs = pkgs: with pkgs; [ ] ++ packages;
+            profile = ''
+              export FHS=1
+              export PROMPT_COMMAND="echo -n '(FHS)'"
+            '';
+          };
+      };
+    };
+}
