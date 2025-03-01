914e7b2e76a6d02fc5164c907c8db665513cb98c
Sense T <me@sense-t.eu.org>
2025-03-01 19:55:55 +0800

    创死了……

--- qzsec/access-client/package.nix
+++ qzsec/access-client/package.nix
@@ -1,9 +1,13 @@
 {
   stdenv,
+  lib,
 
   jq,
   zlib-ng,
   passh,
+  coreutils,
+  openssh,
+  which,
   ...
 }:
 let
@@ -18,16 +22,26 @@ stdenv.mkDerivation rec {
     jq
     zlib-ng
     passh
+    coreutils
+    openssh
+  ];
+
+  nativeBuildInputs = [
+    coreutils
+    which
   ];
 
   phases = [ "installPhase" ];
 
   installPhase = ''
     mkdir -p $out/bin
-    cp -rv "${bin}" $out/bin/${pname}
+    sed "/^export PATH=/s|@PATH|${coreutils}/bin:${jq}/bin:${passh}/bin:${zlib-ng.bin}/bin:${openssh}/bin|" "${bin}" > $out/bin/${pname}
     chmod +x $out/bin/${pname}
 
     mkdir -p $out/share/applications
     sed "/^Version=/s|@VERSION|${version}|" "${desktop}" | sed "/^Exec=/s|@EXEC|$out/bin/${pname}|" > "$out/share/applications/${pname}.desktop"
+
+    echo "PATH: $PATH"
+    which jq && which minideflate && which passh && which ssh || exit 1
   '';
 }
